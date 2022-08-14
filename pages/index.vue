0300b2cda429e078cf5fc615346e90a4f61b295f
VaibhavSys <vaibhavsys@protonmail.com>
2022-08-14 21:02:02 +0530

    feat: Connection Information

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,11 @@
           <h1>Welcome to PumpkinSMP!</h1>
           <p>Welcome to PumpkinSMP! PumpkinSMP is a crossplay Minecraft SMP.</p>
           <img src="@/assets/images/logo.png" />
+        <el-descriptions title="Connection Information">
+          <el-descrption-item label="Hostname">play.pumpkinsmp.fun</el-descrption-item>
+          <el-descrption-item label="Port (Bedrock Edition)">19132</el-descrption-item>
+        </el-descriptions>
+        <b>NOTE: You do not need to add a port for Java Edition, it will figure it out automatically.</b>
         </div>
       </div>
     </el-main>
