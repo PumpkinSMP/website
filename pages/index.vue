ec7b6d49104db879edde402cdbbb1c340cc79731
VaibhavSys <vaibhavsys@protonmail.com>
2022-08-14 22:05:30 +0530

    test

--- pages/index.vue
+++ pages/index.vue
@@ -6,16 +6,17 @@
           <h1>Welcome to PumpkinSMP!</h1>
           <p>Welcome to PumpkinSMP! PumpkinSMP is a crossplay Minecraft SMP.</p>
           <img src="@/assets/images/logo.png" />
-        <el-descriptions title="User Info" border>
-          <el-descriptions-item label="Hostname">play.pumpkinsmp.fun</el-descriptions-item>
-          <el-descriptions-item label="Port (Bedrock Edition)">19132</el-descriptions-item>
-          <el-descriptions-item label="Hosting Location">
-            <el-tag size="small">India</el-tag>
-          </el-descriptions-item>
-          <el-descriptions-item label="Version">1.17-1.19</el-descriptions-item>
-        </el-descriptions>
-
-        <b>NOTE: You do not need to add a port for Java Edition, it will figure it out automatically.</b>
+        <div class="connection-info">
+          <el-descriptions title="User Info" border>
+            <el-descriptions-item label="Hostname">play.pumpkinsmp.fun</el-descriptions-item>
+            <el-descriptions-item label="Port (Bedrock Edition)">19132</el-descriptions-item>
+            <el-descriptions-item label="Hosting Location">
+              <el-tag size="small">India</el-tag>
+            </el-descriptions-item>
+            <el-descriptions-item label="Version">1.17-1.19</el-descriptions-item>
+          </el-descriptions>
+          <b>NOTE: You do not need to add a port for Java Edition, it will figure it out automatically.</b>
+          </div>
         </div>
       </div>
     </el-main>
@@ -29,3 +30,15 @@ export default Vue.extend({
   name: 'IndexPage',
 })
 </script>
+
+<style lang="scss">
+
+.connection-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+</style>
